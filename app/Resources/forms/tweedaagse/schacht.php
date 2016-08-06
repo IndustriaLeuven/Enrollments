5a39d2855d82f90155e6b89e8c181b76651d0e2a
Stefan De Raedemaeker <stefan.deraedemaeker@outlook.com>
2016-08-07 00:18:57 +0200

    General bug fixes
    Used Symphony libraries added
    Made every form element constraint NotBlank
    Fixed Length syntax (I hope)
    Added Birthdate and Vegetaran selections

--- app/Resources/forms/tweedaagse/schacht.php
+++ app/Resources/forms/tweedaagse/schacht.php
@@ -4,41 +4,56 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
+new Symfony\Component\Validator\Constraints\Length;
 
 return function(FormBuilderInterface $formBuilder)
 {
     $formBuilder
-        ->add('First Name*', TextType::class, [
-            'constraints' => [
-                new NotBlank()
-            ],
+        ->add('first_name', TextType::class, ['label'=>'Voornaam',
+            'constraints' => [new NotBlank()],
         ])
 
-        ->add('Last Name*', TextType::class, [
-            'constraints' => [
-                new NotBlank()
-            ],
+        ->add('last_name', TextType::class, ['label'=>'Achternaam',
+            'constraints' => [new NotBlank()],
         ])
 
-        ->add('Group (if you already know',TextType::class,[
+        ->add('Birthdate', DateType::class,['label'=>'Geboortedatum',
+        'constraints'=>[new NotBlank()],
+        ])
+
+        ->add('group',TextType::class,['label'=>'Groep (als je die al weet)','required' => false,
             'constraints'=>[
-                new max(Length(3)),
+                new NotBlank(),
+                new Length(max(3))
             ],
         ])
 
-        ->add('GSM or Phone',TextType::class)
+        ->add('phone',TextType::class,['label'=>'GSM of telefoon nr.',
+        'constraints'=>[new NotBlank()],
+        ])
 
-        ->add('email*', EmailType::class, [
+        ->add('email', EmailType::class, ['label'=>'email',
             'constraints' => [
                 new NotBlank(),
-                new Email(),
+                new Email()
             ],
         ])
 
-        ->add('Comments',TextareaType::class)
+        ->add('vegetarian', CheckboxType::class, ['label'=>'Vegetarisch',
+            'required' => false,
+            'disabled' => true
+        ])
+
+        ->add('Comments', TextareaType::class,['label'=>'Opmerkingen (medicatie, ...', 'required'=> false,
+            'constraints'=>[
+                new NotBlank(),
+        ],
+        ])
 
     ;
 };
