0b126aeae8cd9ccaf2e0f4fac072b868d86b5f23
Stefan De Raedemaeker <stefan.deraedemaeker@outlook.com>
2016-08-07 13:53:22 +0200

    BirthDayType
    Use BirthDayType instead of DateType

--- app/Resources/forms/tweedaagse/schacht.php
+++ app/Resources/forms/tweedaagse/schacht.php
@@ -5,7 +5,6 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -22,7 +21,7 @@ return function(FormBuilderInterface $formBuilder)
             'constraints' => [new NotBlank()],
         ])
 
-        ->add('Birthdate', DateType::class,['label'=>'Geboortedatum',
+        ->add('Birthdate', BirthDayType::class,['label'=>'Geboortedatum',
         'constraints'=>[new NotBlank()],
         ])
 
